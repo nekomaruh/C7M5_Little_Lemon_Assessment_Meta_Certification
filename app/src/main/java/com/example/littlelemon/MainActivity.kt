7478b69be2807b40fef6bdfa637106a292e32824
Jhoan Ordenes Galleguillos <johan.e.galleguillos@accenture.com>
2023-02-10 20:51:11 -0300

    1. Retrieving items from room database

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -47,10 +49,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             LittleLemonTheme {
                 // add databaseMenuItems code here
+                val databaseMenuItems = database.menuItemDao().getAll()
 
                 // add orderMenuItems variable here
+                val orderMenuItems by databaseMenuItems.observeAsState(emptyList())
 
                 // add menuItems variable here
+                MenuItemsList(items = orderMenuItems)
 
                 Column(
                     modifier = Modifier
